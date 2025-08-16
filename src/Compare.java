e4b599c3bcbeecfa32da09047278742171c52e8e
Chipiiiiii <polurutharun1@gmail.com>
2025-08-16 18:16:10 +0530

    Created Compariosn Of Strings

---
+++ src/Compare.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Compare {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an string:-");
+        String a = input.nextLine();
+        System.out.println("Enter an String:-");
+        String b = input.nextLine();
+        String[] c = a.split("\\.");
+        String[] d = b.split("\\.");
+        for (int i = 0; i < Math.min(c.length, d.length); i++) {
+            int e = c[i].compareTo(d[i]);
+            if (e > 0) {
+                System.out.println(a + " is bigger");
+                return;
+            } else if (e < 0) {
+                System.out.println(b + " is bigger");
+                return;
+            }
+        }
+        if (c.length > d.length) {
+            System.out.println(a + " is bigger");
+        } else if (c.length < d.length) {
+            System.out.println(b + " is bigger");
+        } else {
+            System.out.println("Both are equal");
+        }
+    }
+}
