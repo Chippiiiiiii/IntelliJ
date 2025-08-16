49a492addda791404e65ffc0601207b090b2743d
Chipiiiiii <polurutharun1@gmail.com>
2025-08-16 18:16:52 +0530

    Create compress

---
+++ src/Compress.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Compress {
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        String a= input.nextLine();
+        char currentChar = a.charAt(0);
+        int count = 1;
+        String compressed = "";
+        for (int i = 1; i < a.length(); i++) {
+            char nextChar = a.charAt(i);
+            if (nextChar == currentChar) {
+                count++;
+            } else {
+                compressed = compressed + currentChar + count;
+                currentChar = nextChar;
+                count = 1;
+            }
+        }
+        compressed = compressed + currentChar + count;
+        if (compressed.length() < a.length()) {
+            System.out.println("Compressed: " + compressed);
+        } else {
+            System.out.println("Compressed: " + input);
+        }
+    }
+}
